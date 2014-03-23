a11e4df065b54b2656117225ac1de8373b4706f6
Stephen Wan <stephen@stephenwan.net>
2014-03-23 17:07:43 -0500

    syntactic sugar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var events = require('events');
 
 
@@ -5,77 +7,77 @@ var HEADER_TERMINATOR = '\r\n\r\n';
 var LINE_TERMINATOR = '\r\n';
 
 var parseHeader = function(header) {
-	var lines = header.split(LINE_TERMINATOR);
+  var lines = header.split(LINE_TERMINATOR);
 
-	var methodline = lines[0];
-	var method = methodline.split(' ')[0].toUpperCase();
-	var path = methodline.split(' ')[1];
-	var output = {};
+  var methodline = lines[0];
+  var method = methodline.split(' ')[0].toUpperCase();
+  var path = methodline.split(' ')[1];
+  var output = {};
 
-	output.method = method;
-	output.path = path;
+  output.method = method;
+  output.path = path;
 
-	var headers = {};
-	for (var i = 1; i < lines.length; i++) {
-		var line = lines[i];
-		var header = [ line.substr(0, line.indexOf(':')), line.substr(line.indexOf(':') + 1) ];
-		if (header.length == 2) {
-			headers[header[0]] = header[1].replace(/^\s+|\s+$/gm, ''); 
-		} else {
-			output.error = true;
-			output.message = 'invalid header specified (' + header[0] + ')';
-			return output;
-		}
-	}
+  var headers = {};
+  for (var i = 1; i < lines.length; i++) {
+    var line = lines[i];
+    var header = [ line.substr(0, line.indexOf(':')), line.substr(line.indexOf(':') + 1) ];
+    if (header.length == 2) {
+      headers[header[0]] = header[1].replace(/^\s+|\s+$/gm, ''); 
+    } else {
+      output.error = true;
+      output.message = 'invalid header specified (' + header[0] + ')';
+      return output;
+    }
+  }
 
-	// TODO: needs case-insensitivity
-	if (headers.hasOwnProperty('Content-Length')) {
-		output.contentLength = parseInt(headers['Content-Length']);
-		output.hasContent = true;
-	}
-	output.headers = headers;
-	return output;
+  // TODO: needs case-insensitivity
+  if (headers.hasOwnProperty('Content-Length')) {
+    output.contentLength = parseInt(headers['Content-Length']);
+    output.hasContent = true;
+  }
+  output.headers = headers;
+  return output;
 };
 
 var Parser = function(socket) {
-	var self = this;
-	var buffer = new Buffer(0);
-	var collectingContent = -1;
-	var headerData = null;
+  var self = this;
+  var buffer = new Buffer(0);
+  var collectingContent = -1;
+  var headerData = null;
 
-	socket.on('data', function(d) {
-		buffer = Buffer.concat([ buffer, d ]);
-		var strBuffer = buffer.toString();
+  socket.on('data', function(d) {
+    buffer = Buffer.concat([ buffer, d ]);
+    var strBuffer = buffer.toString();
 
-		if (collectingContent == -1 && strBuffer.indexOf(HEADER_TERMINATOR) != -1) {
-			var msgs = strBuffer.split(HEADER_TERMINATOR);
+    if (collectingContent == -1 && strBuffer.indexOf(HEADER_TERMINATOR) != -1) {
+      var msgs = strBuffer.split(HEADER_TERMINATOR);
 
-			// parse first header
-			var headerData = parseHeader(msgs[0]);
-			if (headerData.error) {
-				self.emit('error', headerData.message);
-			} else {
-				buffer = buffer.slice(msgs[0].length + 4);
-				if (!headerData.hasContent) {
-					// done - need to forward request
-					self.emit('message', { headers: headerData.headers, method: headerData.method });
-				} else {
-					// not done - need to get content data
-					collectingContent = headerData.contentLength;
-				}
-			}
-		}
+      // parse first header
+      var headerData = parseHeader(msgs[0]);
+      if (headerData.error) {
+        self.emit('error', headerData.message);
+      } else {
+        buffer = buffer.slice(msgs[0].length + 4);
+        if (!headerData.hasContent) {
+          // done - need to forward request
+          self.emit('message', { headers: headerData.headers, method: headerData.method });
+        } else {
+          // not done - need to get content data
+          collectingContent = headerData.contentLength;
+        }
+      }
+    }
 
-		if (collectingContent >= 0 && collectingContent - buffer.length <= 0) { 
-			// done
-			var content = buffer.slice(0, collectingContent);
-			self.emit('message', { headers: headerData.headers, method: headerData.method, content: content });
+    if (collectingContent >= 0 && collectingContent - buffer.length <= 0) { 
+      // done
+      var content = buffer.slice(0, collectingContent);
+      self.emit('message', { headers: headerData.headers, method: headerData.method, content: content });
 
-			buffer = buffer.slice(collectingContent + 1);
+      buffer = buffer.slice(collectingContent + 1);
 
-			collectingContent = -1;
-		}
-	});
+      collectingContent = -1;
+    }
+  });
 };
 
 Parser.prototype.__proto__ = events.EventEmitter.prototype;
--- test/parser.js
+++ test/parser.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var assert = require('assert');
 var stream = require('stream');
 var Parser = require('../index');
@@ -5,62 +7,62 @@ var Parser = require('../index');
 describe('Parser', function() {
 	describe('#Parser', function() {
 		it('should parse a request without content', function(done) {
-	    	var mockStream = new stream.PassThrough();
-	    	var parser = new Parser(mockStream);
-	    	parser.on('message', function(m) {
-	    		assert(m.method == 'GET');
-	    		assert(m.headers['Data'] == 'Hello');
-	    		assert(m.headers['Data-2'] == 'More Hello');
-	    		done();
-	    	});
-	    	mockStream.write('GET /test HTTP/1.1\r\nData:Hello\r\nData-2:More Hello\r\n\r\n');
+			var mockStream = new stream.PassThrough();
+			var parser = new Parser(mockStream);
+			parser.on('message', function(m) {
+				assert(m.method == 'GET');
+				assert(m.headers['Data'] == 'Hello');
+				assert(m.headers['Data-2'] == 'More Hello');
+				done();
+			});
+			mockStream.write('GET /test HTTP/1.1\r\nData:Hello\r\nData-2:More Hello\r\n\r\n');
 		});
 
 		it('should parse a request with content', function(done) {
-	    	var mockStream = new stream.PassThrough();
-	    	var parser = new Parser(mockStream);
-	    	parser.on('message', function(m) {
-	    		assert(m.content == 'dat content');
-	    		done();
-	    	});
+			var mockStream = new stream.PassThrough();
+			var parser = new Parser(mockStream);
+			parser.on('message', function(m) {
+				assert(m.content == 'dat content');
+				done();
+			});
 
-	    	var content = 'dat content';
-	    	mockStream.write('GET /test HTTP/1.1\r\nData:Hello\r\nContent-Length:' + content.length + '\r\n\r\n' + content);
+			var content = 'dat content';
+			mockStream.write('GET /test HTTP/1.1\r\nData:Hello\r\nContent-Length:' + content.length + '\r\n\r\n' + content);
 		});
 
 		it('should parse multiple http requests without content', function(done) {
-	    	var mockStream = new stream.PassThrough();
-	    	var parser = new Parser(mockStream);
+			var mockStream = new stream.PassThrough();
+			var parser = new Parser(mockStream);
 
-	    	var expected = [ 'Hello', 'Goodbye' ];
+			var expected = [ 'Hello', 'Goodbye' ];
 
-	    	parser.on('message', function(m) {
-	    		assert(expected.shift() == m.headers['Data']);
+			parser.on('message', function(m) {
+				assert(expected.shift() == m.headers['Data']);
 
-	    		if (expected.length == 0)
-		    		done();
-	    	});
+				if (expected.length == 0)
+					done();
+			});
 
-	    	mockStream.write('GET / HTTP/1.1\r\nData:Hello\r\n\r\n');
-	    	mockStream.write('GET / HTTP/1.1\r\nData:Goodbye\r\n\r\n');
+			mockStream.write('GET / HTTP/1.1\r\nData:Hello\r\n\r\n');
+			mockStream.write('GET / HTTP/1.1\r\nData:Goodbye\r\n\r\n');
 		});
 
 		it('should parse multiple http requests with content', function(done) {
-	    	var mockStream = new stream.PassThrough();
-	    	var parser = new Parser(mockStream);
+			var mockStream = new stream.PassThrough();
+			var parser = new Parser(mockStream);
 
-	    	var expected = [ 'Hello', 'Goodbye' ];
+			var expected = [ 'Hello', 'Goodbye' ];
 
-	    	parser.on('message', function(m) {
-	    		assert(expected.shift() == m.content);
+			parser.on('message', function(m) {
+				assert(expected.shift() == m.content);
 
-	    		if (expected.length == 0)
-		    		done();
-	    	});
+				if (expected.length == 0)
+					done();
+			});
 
-	    	mockStream.write('GET / HTTP/1.1\r\nContent-Length:' + expected[0].length + '\r\n\r\n' + expected[0]);
-	    	mockStream.write('GET / HTTP/1.1\r\nContent-Length:' + expected[0].length + '\r\n\r\n' + expected[0]);
-	    });
+			mockStream.write('GET / HTTP/1.1\r\nContent-Length:' + expected[0].length + '\r\n\r\n' + expected[0]);
+			mockStream.write('GET / HTTP/1.1\r\nContent-Length:' + expected[0].length + '\r\n\r\n' + expected[0]);
+		});
 
 	});
 });
