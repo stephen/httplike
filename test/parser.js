e555287d14a3af15d280a1a19df0adf2a486a303
Stephen Wan <stephen@stephenwan.net>
2014-04-19 14:12:53 -0500

    added tests for parseHeader, exposed parseHeader under Parser module

--- test/parser.js
+++ test/parser.js
@@ -5,7 +5,7 @@ var stream = require('stream');
 var Parser = require('../index');
 
 describe('Parser', function() {
-	describe('#Parser', function() {
+	describe('#Parser e2e', function() {
 		it('should parse a request without content', function(done) {
 			var mockStream = new stream.PassThrough();
 			var parser = new Parser(mockStream);
@@ -63,6 +63,25 @@ describe('Parser', function() {
 			mockStream.write('GET / HTTP/1.1\r\nContent-Length:' + expected[0].length + '\r\n\r\n' + expected[0]);
 			mockStream.write('GET / HTTP/1.1\r\nContent-Length:' + expected[0].length + '\r\n\r\n' + expected[0]);
 		});
+	});
+	describe('#parseHeader', function() {
+		it('should parse headers', function() {
+			var headerData = Parser.parseHeader('GET /test/path HTTP/1.1\r\nData:Hello\r\nData-2:More Hello');
+			assert(headerData.path == '/test/path');
+			assert(headerData.method == 'GET');
+			assert(headerData.headers['Data'] == 'Hello');
+			assert(headerData.headers['Data-2'] == 'More Hello');
+			assert(!headerData.hasContent);
+		});
 
+		it('should should fail on malformed headers', function() {
+			assert(Parser.parseHeader('GET /test/path HTTP/1.1\r\nData\r\nData-2:More Hello').error);
+		});
+
+		it('should handle multiple colons in header', function() {
+			var headerData = Parser.parseHeader('GET /test/path HTTP/1.1\r\nData:Hello:There\r\nData-2:More Hello');
+			assert(headerData.headers['Data'] == 'Hello:There');
+			assert(headerData.headers['Data-2'] == 'More Hello');
+		});
 	});
 });
